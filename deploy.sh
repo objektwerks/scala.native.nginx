f4fdcd562bd43483407ea63fd1f40efdbbdc66dc
objektwerks <objektwerks@runbox.com>
2021-10-29 05:29:13 -0400

    edited deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/zsh
 
-curl -X PUT --verbose --data-binary @config.json \
-   --unix-socket /opt/homebrew/var/run/unit/control.sock \
-   http://localhost/config
+curl -X PUT --unix-socket control.sock -d @config.json localhost/config
