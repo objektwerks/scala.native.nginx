11fa359dd079b8c6d9bd17f025fe4597a42f18f2
objektwerks <objektwerks@runbox.com>
2023-08-31 16:36:54 -0400

    refactored now server

--- src/main/scala/objektwerks/NowServer.scala
+++ src/main/scala/objektwerks/NowServer.scala
@@ -3,25 +3,15 @@ package objektwerks
 import java.time.Instant
 
 import snunit.*
-import snunit.routes.*
 
-import trail.*
-
-object NowServer {
-  def main(args: Array[String]): Unit = {
-    val route  = Root / "now"
+object NowServer:
+  def main(args: Array[String]): Unit =
     AsyncServerBuilder
-      .withRequestHandler(
-        _.withMethod(Method.GET)
-          .withRoute(route) { case (request, _) =>
-            request.send(
-              statusCode = StatusCode.OK,
-              content = s"*** Now: ${Instant.now().toString}",
-              headers = Seq("Content-Type" -> "text/plain")
-            )
-          }
+      .setRequestHandler(request =>
+        request.send(
+          statusCode = StatusCode.OK,
+          content = s"*** Now: ${Instant.now().toString}",
+          headers = Seq("Content-Type" -> "text/plain")
+        )
       )
       .build()
-    ()
-  }
-}
