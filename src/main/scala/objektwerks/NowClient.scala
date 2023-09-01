4440fb3142563d606bee7568d9d1b8966d31d9ce
objektwerks <objektwerks@runbox.com>
2023-09-01 12:43:05 -0400

    enhanced now client

--- src/main/scala/objektwerks/NowClient.scala
+++ src/main/scala/objektwerks/NowClient.scala
@@ -7,9 +7,13 @@ import sttp.model.Uri
 
 object NowClient:
   def main(args: Array[String]): Unit =
+    // See config.json for server listeners.
     val url = if args.isEmpty then uri"http://localhost:7474/" else Uri(args(0))
+    println( s"*** Now Client sending request to: ${url.toString}" )
+
     val backend = CurlBackend()
     val response: Response[String] = quickRequest
       .get(url)
       .send(backend)
+    
     println( s"*** Now Client response: ${response.body}" )
